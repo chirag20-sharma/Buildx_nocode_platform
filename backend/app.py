b7daf8cc957f9bb39e727f97fdef28a7b1a79b79
Chirag Sharma <2303031241246@paruluniversity.ac.in>
2026-01-25 17:19:33 +0530

    Add Flask routes and MongoDB test endpoint

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from db import users_collection
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "BuildX Backend Running with MongoDB"
+
+@app.route("/test-db")
+def test_db():
+    users_collection.insert_one({
+        "name": "Test User",
+        "email": "test@buildx.com"
+    })
+    return "MongoDB Connected & Inserted"
+
+if __name__ == "__main__":
+    app.run(debug=True)
